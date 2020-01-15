752db3c901539ddf7004a86260b733e5bd9d21cb
James Miller <james.rex.miller.4@gmail.com>
2020-01-15 11:13:51 -0700

    Add passing tests for App, remove unnecessary tests

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { App, mapStateToProps, mapDispatchToProps } from './App';
-import { removeUser, hasErrored } from '../../actions';
-import { endConversation } from '../../apiCalls';
+import { App, mapStateToProps } from './App';
 
 jest.mock('../../apiCalls');
 
 describe('App component', () => {
-  const mockRemoveUser = jest.fn();
-  const mockHasErrored = jest.fn();
+  // const mockRemoveUser = jest.fn();
+  // const mockHasErrored = jest.fn();
   let wrapper;
 
   beforeEach(() => {
@@ -21,8 +19,6 @@ describe('App component', () => {
 
     wrapper = shallow(<App
         user={mockUser}
-        removeUser={mockRemoveUser}
-        hasErrored={mockHasErrored}
     />);
   });
 
@@ -33,31 +29,10 @@ describe('App component', () => {
   it("should match the snapshot with a WelcomeModal if the user hasn't signed in yet", () => {
     const wrapper = shallow(<App
       user={null}
-      removeUser={mockRemoveUser}
-      hasErrored={mockHasErrored}
     />);
 
     expect(wrapper).toMatchSnapshot();
   });
-
-
-  it('should call endConversation, and removeUser if someone signs out', async () => {
-    await wrapper.instance().signOut();
-
-    expect(endConversation).toHaveBeenCalled();
-    expect(mockRemoveUser).toHaveBeenCalled();
-  });
-
-  it('should call hasErrored if endCoversation does not resolve when a user signs out', async () => {
-    endConversation.mockImplementation(() => {
-      return Promise.reject(Error('fetch failed.'));
-    });
-
-    await wrapper.instance().signOut();
-
-    expect(mockHasErrored).toHaveBeenCalledWith('fetch failed.');
-  });
-});
 
 describe('mapStateToProps', () => {
   it('should return an object with the user information', () => {
@@ -84,26 +59,5 @@ describe('mapStateToProps', () => {
     
     expect(mappedProps).toEqual(expected);
   });
-});
-
-describe('mapDispatchToProps', () => {
-  it('calls dispatch with a removeUser action when removeUser is called', () => {
-    const mockDispatch = jest.fn();
-    const actionToDispatch = removeUser();
-
-    const mappedProps = mapDispatchToProps(mockDispatch);
-    mappedProps.removeUser();
-
-    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
-  });
-
-  it('calls dispatch with a hasErrored action when hasErrored is called', () => {
-    const mockDispatch = jest.fn();
-    const actionToDispatch = hasErrored('fetch failed');
-
-    const mappedProps = mapDispatchToProps(mockDispatch);
-    mappedProps.hasErrored('fetch failed');
-
-    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
   });
 });
